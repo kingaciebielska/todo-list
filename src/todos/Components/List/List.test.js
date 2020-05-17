ad2d2459dfc5b8d4c4104988e2892f10c369fc97
Kinga Ciebielska <elfiku@vp.pl>
2020-05-17 21:28:18 +0200

    Fixing code styling and removing unused file

--- src/todos/Components/List/List.test.js
+++ src/todos/Components/List/List.test.js
@@ -13,38 +13,37 @@ describe('<List />', ()  => {
   it('should render empty list', () => {
     const list = []
     const wrapper = mount(<List list={list} handleRemove={handleRemove}/>)
-      expect(wrapper.props().list).toEqual([])
+    expect(wrapper.props().list).toEqual([])
   })
 
   it('should render not empty list', () => {
     const list = [{id: '1', title: 'lorem', description: 'ipsum'}]
     const wrapper = mount(<List list={list} handleRemove={handleRemove}/>)
-      expect(wrapper.props().list).toEqual([{id: '1', title: 'lorem', description: 'ipsum'}])
+    expect(wrapper.props().list).toEqual([{id: '1', title: 'lorem', description: 'ipsum'}])
   })
 
   it('should render title', () => {
     const list = [{id: '1', title: 'lorem', description: 'ipsum'}]
     const wrapper = mount(<List list={list} handleRemove={handleRemove}/>)
-      expect(wrapper.find('h5').text()).toEqual('lorem')
+    expect(wrapper.find('h5').text()).toEqual('lorem')
   })
 
   it('should render description', () => {
     const list = [{id: '1', title: 'lorem', description: 'ipsum'}]
     const wrapper = mount(<List list={list} handleRemove={handleRemove}/>)
-      expect(wrapper.find('p').text()).toEqual('ipsum')
+    expect(wrapper.find('p').text()).toEqual('ipsum')
   })
 
   it('should render correct number of items', () => {
     const list = [{id: '1', title: 'lorem', description: 'ipsum'}]
     const wrapper = mount(<List list={list} handleRemove={handleRemove}/>)
-      expect(wrapper.find('li')).toHaveLength(1)
+    expect(wrapper.find('li')).toHaveLength(1)
   })
 
   it('remove item', () => {
     const list = [{id: '123', title: 'lorem', description: 'ipsum'}]
 		const wrapper = mount(<List list={list} handleRemove={handleRemove}/>);
     wrapper.find('button').simulate('click')
-
 		expect(handleRemove).toHaveBeenCalledTimes(1)
 		expect(handleRemove).toHaveBeenLastCalledWith('123')
 	})
